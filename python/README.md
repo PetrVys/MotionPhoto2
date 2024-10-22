9eb471abf4c7fa505ef25f5a56ea2ed8ee7652a7
Tkd-Alex <alex.tkd.alex@gmail.com>
2024-10-22 12:10:31 +0200

    Python implementation of MotionPhoto2, make it cross-platform

---
+++ python/README.md
@@ -0,0 +1,32 @@
+## Installation
+
+Please make sure to have ExifTool [ExifTool](https://exiftool.org/) installed on your computer.
+
+Install dependeces `pip install -r requirements.txt`
+
+## Usage
+
+### Individual photos
+
+To convert image and video pair to Motion Photo v2, run:
+
+```
+> python main.py --input-image ImageFile.HEIC --input-video VideoFile.MP4
+```
+
+### Directory mode
+The script will automatically search file in directory that have the same name but different extension, for example: IMG_1496.HEIC, IMG_1496.MP4
+```
+> python main.py --input-directory /your/directory
+```
+
+### Notes
+- The output of new images file will be: original_name.**LIVE**.ext
+- If you provide a `--output-directory` the file will be: **output-directory**/original_name.ext
+- While the script mux the images and video two temps files will be created and deleted automatically, you can keep it with `--keep-temp`
+- If you want to remplace the original image file with the live one use: `--overwrite` (use at your risk)
+- If you want to remove the video file after muxing use: `--delete-video` (use at your risk)
+
+#### Documentation
+- https://developer.android.com/media/platform/motion-photo-format
+- https://medium.com/android-news/working-with-motion-photos-da0aa49b50c
---
+++ python/main.py
@@ -0,0 +1,126 @@
+import argparse
+import sys
+import os
+
+from pathlib import Path
+
+from Muxer import Muxer
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="MotionPhoto2",
+        description="Mux HEIC and JPG Live Photos into Google/Samsung Motion Photos",
+    )
+
+    parser.add_argument("-ii", "--input-image", help="Input file image (.heic, .jpg)")
+    parser.add_argument("-iv", "--input-video", help="Input file video (.mov, .mp4)")
+    parser.add_argument("-of", "--output-file", help="Output filename of Live Photos")
+
+    parser.add_argument(
+        "-id", "--input-directory", help="Mux all the photos and video in directory"
+    )
+    parser.add_argument(
+        "-od",
+        "--output-directory",
+        help="Store all the Live Photos into dedicated directory",
+    )
+
+    parser.add_argument(
+        "-dv",
+        "--delete-video",
+        action="store_true",
+        help="Automatically delete video after muxing",
+    )
+    parser.add_argument(
+        "-kt",
+        "--keep-temp",
+        action="store_true",
+        help="Keep temp file used during muxing",
+    )
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="Overwrite the original image file as output Live Photos",
+    )
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose muxing")
+
+    args = parser.parse_args()
+
+    if args.input_directory is not None and (
+        args.input_image is not None or args.input_video is not None
+    ):
+        print("[ERROR] Input directory cannot be use with input-image or input-video")
+        sys.exit(1)
+
+    if args.input_directory is None:
+        if args.input_image is None or args.input_video is None:
+            print("[ERROR] Please provide both input image/video or input directory")
+            sys.exit(1)
+
+    if args.output_directory is not None and args.overwrite is True:
+        print("[ERROR] Output directory cannot be use overwrite option")
+        sys.exit(1)
+
+    if args.output_file is not None and args.overwrite is True:
+        print("[ERROR] Output file cannot be use overwrite option")
+        sys.exit(1)
+
+    if args.overwrite is True or args.delete_video is True:
+        text = f"[WARNING] Make sure to have a backup of your image and/or video file (overwrite={args.overwrite}, delete-video={args.delete_video})"
+        confirmation = input(f"{text}\nContinue? [Y/n] ")
+        if len(confirmation) > 0 and confirmation[0].lower() == "n":
+            sys.exit(1)
+
+    if args.output_directory is not None:
+        output_directory = f"{Path(args.output_directory).resolve()}"
+        if os.path.exists(output_directory) is False:
+            os.mkdir(output_directory)
+
+    if args.input_directory is not None:
+        input_directory = f"{Path(args.input_directory).resolve()}"
+        # Going to search couples of file with ext (".heic", ".heif", ".avif", ".jpg", ".jpeg") (".mp4", ".mov")
+        files = os.listdir(input_directory)
+        videos = [
+            f
+            for f in files
+            if os.path.isfile(os.path.join(input_directory, f))
+            and Path(f).suffix.lower() in [".mp4", ".mov"]
+        ]
+        images = [
+            f
+            for f in files
+            if os.path.isfile(os.path.join(input_directory, f))
+            and Path(f).suffix.lower() in [".heic", ".heif", ".avif", ".jpg", ".jpeg"]
+        ]
+
+        for image in images:
+            fname = Path(image).stem
+            for ext in [".mp4", ".mov", ".MP4", ".MOV"]:
+                if fname + ext in videos:
+                    video = videos.pop(videos.index(fname + ext))
+
+                    input_image = os.path.join(input_directory, image)
+                    input_video = os.path.join(input_directory, video)
+
+                    Muxer(
+                        image_fpath=input_image,
+                        video_fpath=input_video,
+                        output_directory=args.output_directory,
+                        delete_video=args.delete_video,
+                        delete_temp=not args.keep_temp,
+                        overwrite=args.overwrite,
+                        verbose=args.verbose,
+                    ).mux()
+                    break
+    else:
+        Muxer(
+            image_fpath=args.input_image,
+            video_fpath=args.input_video,
+            output_fpath=args.output_file,
+            output_directory=args.output_directory,
+            delete_video=args.delete_video,
+            delete_temp=not args.keep_temp,
+            overwrite=args.overwrite,
+            verbose=args.verbose,
+        ).mux()
