b43f624906d329e751bbab50000eb41ad18517ff
Sahil Phule <sahilph@users.noreply.github.com>
2025-03-31 20:13:56 -0400

    Add video signature constants

--- constants.py
+++ constants.py
@@ -73,3 +73,8 @@ SAMSUNG_TAG_IDS = {
 }
 
 SAMSUNG_SEFH_VERSION = 107
+
+VIDEO_SIGNATURE = {
+    "MOV": [sig.encode('iso-8859-1') for sig in ["ftypqt", "wide", "free", "mdat", "pnot", "skip"]],
+    "MP4": [sig.encode('iso-8859-1') for sig in ["ftypMSNV", "ftypisom"]]
+}
