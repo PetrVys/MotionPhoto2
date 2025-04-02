e862483c854210f24700aa553ec7e419e2bc672b
Sahil Phule <sahilph@users.noreply.github.com>
2025-04-02 00:09:30 -0400

    Compare only videos

--- utils.py
+++ utils.py
@@ -68,14 +68,12 @@ def extract_video_from_image(fpath: str, et: exiftool.ExifToolHelper) -> bytes:
 def input_output_binary_compare(input_image: str, input_video: str, output_image: str) -> bool:
     if all((os.path.exists(input_image), os.path.exists(input_video), os.path.exists(output_image))):
         try:
-            with open(input_image, "rb") as i_img:
-                input_image_data = i_img.read()
-                with open(input_video, "rb") as i_vid:
-                    input_video_data = i_vid.read()
-                    with open(output_image, "rb") as o_img:
-                        output_image_data = o_img.read()
-                        if output_image_data.find(input_image_data) and output_image_data.find(input_video_data):
-                            return True
+            with open(input_video, "rb") as i_vid:
+                input_video_data = i_vid.read()
+                with open(output_image, "rb") as o_img:
+                    output_image_data = o_img.read()
+                    if output_image_data.find(input_video_data) != -1:
+                        return True
         except:
             pass
     return False
